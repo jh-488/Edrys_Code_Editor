ccf68dc4dafbe88c135d8543fbe9833b73777a45
jh-488 <jihadhyadi01@gmail.com>
2024-03-29 16:58:09 +0100

    server can handle challenges and normal uplaods

--- server/controllers/sketchControllers.js
+++ server/controllers/sketchControllers.js
@@ -1,6 +1,9 @@
 // Import spawn to execute external commands
 const { spawn } = require("child_process");
 
+const fs = require("fs");
+const { startSerialPortListener } = require("./serialPortListener");
+
 // Compile an arduino sketch from an .ino file
 // Change the "arduino:avr:uno" with your arduino core
 compileSketch = (sketchPath) => {
@@ -63,7 +66,72 @@ uploadSketch = (port, sketchPath) => {
 };
 
 
+
+// Path where the sketch is stored
+const sketchPath = "./sketch/sketch.ino";
+
+// Serial port where the arduino is connected
+const args = process.argv.slice(2);
+const portIndex = args.indexOf("--port");
+const BOARD_PORT = portIndex !== -1 ? args[portIndex + 1] : null;
+
+if (!BOARD_PORT) {
+  console.error("Error: Please provide a serial port using --port <port>");
+  process.exit(1);
+}
+
+// Compile and upload the sketch after receiving the data 
+// if the sketch is of type challenge, run the tests. Else just compile and upload
+const compileAndUploadSketch = async (sketch, ws, isChallenge) => {
+  // Write the sketch to the file
+  fs.writeFileSync(sketchPath, sketch, "utf8");
+
+  try {
+    // Compile the sketch and send the response to the client
+    const {
+      message: compileMessage,
+      stdout: compileStdout,
+      stderr: compileStderr,
+    } = await compileSketch(sketchPath);
+    ws.send(
+      JSON.stringify({
+        message: compileMessage,
+        stdout: compileStdout,
+        stderr: compileStderr,
+      })
+    );
+
+    // Upload the sketch and send the response to the client
+    const {
+      message: uploadMessage,
+      stdout: uploadStdout,
+      stderr: uploadStderr,
+    } = await uploadSketch(BOARD_PORT, sketchPath);
+    ws.send(
+      JSON.stringify({
+        message: uploadMessage,
+        stdout: uploadStdout,
+        stderr: uploadStderr,
+      })
+    );
+
+    if (isChallenge) {
+      // Check if all tests passed and send the response to the client
+      const allTestsPassed = await startSerialPortListener(BOARD_PORT);
+      if (allTestsPassed) {
+        ws.send(JSON.stringify({ testMessage: "All tests passed. You solved the challenge", testPassed: true }));
+      } else {
+        ws.send(JSON.stringify({ testMessage: "Some tests failed. Try again", testPassed: false }));
+      }
+    }
+  } catch (error) {
+    console.error(error);
+
+    // Send error response to client
+    ws.send(JSON.stringify({ error: error.message + error.stderr }));
+  }
+};
+
 module.exports = {
-    compileSketch,
-    uploadSketch
+    compileAndUploadSketch
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-
 const WEB_SOCKET_PORT = process.env.WEB_SOCKET_PORT || 8080;
 
 // Websocket server configuration
@@ -8,30 +6,14 @@ const WebSocketServer = new WebSocket.Server({ port: WEB_SOCKET_PORT });
 
 // import compile and upload functions
 const {
-  compileSketch,
-  uploadSketch,
+  compileAndUploadSketch
 } = require("./controllers/sketchControllers");
-
-// import serial port listener
-const { startSerialPortListener } = require("./controllers/serialPortListener");
 
 // import tests
 const { testFunctions } = require("./challengesTests/challengesTests");
 
-// Serial port where the arduino is connected
-const args = process.argv.slice(2);
-const portIndex = args.indexOf("--port");
-const BOARD_PORT = portIndex !== -1 ? args[portIndex + 1] : null;
 
-if (!BOARD_PORT) {
-  console.error("Error: Please provide a serial port using --port <port>");
-  process.exit(1);
-}
-
-// Path where the sketch is stored
-const sketchPath = "./sketch/sketch.ino";
-
-// Compile and upload the sketch after receiving the data (code + port name)
+// Compile and upload the sketch after receiving the data 
 WebSocketServer.on("connection", (ws) => {
   console.log("New client connected");
 
@@ -44,53 +26,9 @@ WebSocketServer.on("connection", (ws) => {
 
     // Check if the challenge test is available
     if (testSketch) {
-      // Write the sketch to the file
-      fs.writeFileSync(sketchPath, testSketch, "utf8");
-
-      try {
-        // Compile the sketch and send the response to the client
-        const {
-          message: compileMessage,
-          stdout: compileStdout,
-          stderr: compileStderr,
-        } = await compileSketch(sketchPath);
-        ws.send(
-          JSON.stringify({
-            message: compileMessage,
-            stdout: compileStdout,
-            stderr: compileStderr,
-          })
-        );
-
-        // Upload the sketch and send the response to the client
-        const {
-          message: uploadMessage,
-          stdout: uploadStdout,
-          stderr: uploadStderr,
-        } = await uploadSketch(BOARD_PORT, sketchPath);
-        ws.send(
-          JSON.stringify({
-            message: uploadMessage,
-            stdout: uploadStdout,
-            stderr: uploadStderr,
-          })
-        );
-
-        // Check if all tests passed and send the response to the client
-        const allTestsPassed = await startSerialPortListener(BOARD_PORT);
-        if (allTestsPassed) {
-          ws.send(JSON.stringify({ testMessage: "All tests passed. You solved the challenge", testPassed: true }));
-        } else {
-          ws.send(JSON.stringify({ testMessage: "Some tests failed. Try again", testPassed: false }));
-        }
-      } catch (error) {
-        console.error(error);
-
-        // Send error response to client
-        ws.send(JSON.stringify({ error: error.message + error.stderr }));
-      }
+      compileAndUploadSketch(testSketch, ws, true);
     } else {
-      ws.send(JSON.stringify({ error: "Test for this challenge was not found!!" }));
+      compileAndUploadSketch(code, ws, false);
     }
   });
 
@@ -98,5 +36,6 @@ WebSocketServer.on("connection", (ws) => {
     console.log("Client disconnected");
   });
 });
+
 
 console.log(`Websocket server started at port ${WEB_SOCKET_PORT}`);
